c65eaf445a98950c2fe32b829d64097bd365e847
rubenwardy <rubenwardy@gmail.com>
2016-07-27 23:49:28 +0100

    Get conditional from stack to the right of the tool, add same conditional

--- init.lua
+++ init.lua
@@ -1,6 +1,21 @@
-minetest.register_craftitem("powertools:down_stack_digger", {
-	description = "Down stack Digger",
-	inventory_image = "powertools_down_stack_digger.png",
+local function getStackToTheRight(player)
+	local idx = player:get_wield_index()
+	local inv = minetest.get_inventory({
+		type = "player",
+		name = player:get_player_name()
+	})
+	if idx < player:hud_get_hotbar_itemcount() then
+		local stack = inv:get_stack("main", idx + 1)
+		return stack and stack:get_name()
+	else
+		return nil
+	end
+end
+
+
+minetest.register_craftitem("powertools:digger_down_column", {
+	description = "Down Column Digger\nDigs tool.stackcount downwards, including punched node",
+	inventory_image = "powertools_digger_down_column.png",
 	on_use = function(itemstack, user, pointed_thing)
 		if pointed_thing.type == "node" then
 			local node = minetest.get_node(pointed_thing.under)
@@ -15,21 +30,55 @@ minetest.register_craftitem("powertools:down_stack_digger", {
 	end
 })
 
-minetest.register_craftitem("powertools:down_stack_digger_conditional", {
-	description = "Conditional Down Stack Digger",
-	inventory_image = "powertools_down_stack_digger_conditional.png",
+minetest.register_craftitem("powertools:digger_down_column_conditional_start", {
+	description = "Conditional (Start) Down Stack Digger\n" ..
+		"Digs tool.stackcount downwards, including punched node\n" ..
+		"Only works if the punched node is the same as the stack to the right of the tool.",
+	inventory_image = "powertools_digger_down_column_conditional_start.png",
 	on_use = function(itemstack, user, pointed_thing)
 		if pointed_thing.type == "node" then
-			local condition = "default:dirt_with_grass"
+			local condition = getStackToTheRight(user)
+			print(dump(condition))
 			local node = minetest.get_node(pointed_thing.under)
-			if not condition or node.name == condition then
+			if not condition then
+				minetest.chat_send_player(user:get_player_name(), "Please put a node stack to the right of this tool to set the start condition")
+			elseif node.name == condition then
 				local pos = pointed_thing.under
 				for i=1, itemstack:get_count() do
 					minetest.set_node(pos, {name="air"})
 					pos.y = pos.y - 1
 				end
-			else
+			elseif minetest.registered_nodes[condition] then
 				minetest.chat_send_player(user:get_player_name(), "Please punch a node of type " .. condition)
+			else
+				minetest.chat_send_player(user:get_player_name(), "Please put a valid node stack to the right of this tool. (" ..
+					condition .. " is not a valid node)")
+			end
+		else
+			minetest.chat_send_player(user:get_player_name(), "Please punch a node")
+		end
+	end
+})
+
+minetest.register_craftitem("powertools:digger_down_column_conditional_same", {
+	description = "Conditional (Same) Down Stack Digger\n" ..
+		"Digs tool.stackcount downwards, including punched node\n" ..
+		"Only removes nodes that are the same as the stack to the right of the tool",
+	inventory_image = "powertools_digger_down_column_conditional_same.png",
+	on_use = function(itemstack, user, pointed_thing)
+		if pointed_thing.type == "node" then
+			local condition = getStackToTheRight(user)
+			local node = minetest.get_node(pointed_thing.under)
+			if condition then
+				local pos = pointed_thing.under
+				for i=1, itemstack:get_count() do
+					if minetest.get_node(pos).name == condition then
+						minetest.set_node(pos, {name="air"})
+					end
+					pos.y = pos.y - 1
+				end
+			else
+				minetest.chat_send_player(user:get_player_name(), "Please put a node stack to the right of this tool to set the condition")
 			end
 		else
 			minetest.chat_send_player(user:get_player_name(), "Please punch a node")
